7342d186cbb9f5d7f6736077b6a81dc5b41a5152
李良 <13273910807@163.com>
2022-05-20 16:57:40 +0800

    提交输出启动时间

--- src/main/java/com/ll/snowflake/SnowflakeApplication.java
+++ src/main/java/com/ll/snowflake/SnowflakeApplication.java
@@ -3,11 +3,14 @@ package com.ll.snowflake;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Date;
+
 @SpringBootApplication
 public class SnowflakeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SnowflakeApplication.class, args);
+        System.out.println("项目启动完成" + new Date());
     }
 
 }
